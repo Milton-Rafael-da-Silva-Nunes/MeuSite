badf0ce1ef8810b28c4e6223b92cd07028ad2846
Rafael Nunes <rafa.nuns17@gmail.com>
2025-01-08 17:44:18 -0300

    Mais sobre Parametros - Props - Componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
 
-const BemVindo = (props) => {
-    return (
+const Equipe = (props) => {
+    return(
         <div>
-            <h2>Bem-vindo(a) {props.nome}</h2>
-            <h3>Tenho {props.idade} anos.</h3>
+            <Sobre userName={props.nome} cargo={props.cargo} idade={props.idade}/>
+            <MediaSocial fb={props.facebook}/>
+            <hr/>
+        </div>
+    );
+}
+
+const Sobre = (props) => {
+    return(
+        <div>
+            <h2>Olá sou o(a) {props.userName}</h2>
+            <h4>Cargo: {props.cargo}</h4>
+            <h4>idade: {props.idade}</h4>
+        </div>
+    );
+}
+
+const MediaSocial = (props) => {
+    return(
+        <div>
+            <a href={props.fb}>Facebook </a>
+            <a>LinkedIn </a>
+            <a>YouTube </a>
         </div>
     );
 }
@@ -12,9 +33,10 @@ const BemVindo = (props) => {
 function App() {
     return (
         <div>
-            Olá Mundo!
-            <BemVindo nome ="Rafael" idade="28"/>
-            <BemVindo nome ="José" idade="18"/>
+            <h1>Conheça nossa Equipe:</h1>    
+            <Equipe nome="Rafael" cargo="Programador" idade="28" facebook="http://google.com.br"/>  
+            <Equipe nome="José Maria" cargo="Agricultor" idade="65" facebook="http://google.com.br"/> 
+            <Equipe nome="Maria Clara" cargo="Manicure" idade="32" facebook="http://google.com.br"/>     
         </div>
     );
 }
