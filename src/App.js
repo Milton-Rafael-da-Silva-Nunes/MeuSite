d710904b76987e2ca034231e4166f898cb8a007f
rafael_nunes <rafa.nuns17@gmail.com+>
2025-01-08 23:21:48 -0300

    Trabalhando com STATES

--- src/App.js
+++ src/App.js
@@ -1,48 +1,51 @@
 import React, { Component } from "react";
 
-class Equipe extends Component {
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            nome: 'Rafael',
+            contador: 0
+        };
+
+        this.aumentar = this.aumentar.bind(this);
+        this.diminuir = this.diminuir.bind(this);
+    }
+
+    aumentar() {
+        let state = this.state;
+        state.contador += 1;
+        this.setState(state);
+        state.nome = "Jose";
+    }
+
+    diminuir() {
+        let state = this.state;
+        if(state.contador === 0) {
+            alert('Não pode ser menor que Zero!');
+            return;
+        }
+
+        state.contador -= 1;
+        this.setState(state);
+    }
+
     render() {
         return (
             <div>
-                <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade} />
-                <Social />
-                <hr />
+                <h1>Contador</h1>
+                {this.state.nome}
+
+                <h3>
+                    <button onClick={this.diminuir}> - </button>
+                    {this.state.contador}
+                    <button onClick={this.aumentar}> + </button>
+                </h3>
+
             </div>
         );
     }
-}
-
-class Sobre extends Component {
-    render() {
-        return (
-            <div>
-                <h2>olá sou o(a) {this.props.nome} </h2>
-                <h3>Cargo: {this.props.cargo} </h3>
-                <h3>Idade: {this.props.idade} </h3>
-            </div>
-        );
-    }
-}
-
-const Social = () => {
-    return (
-        <div>
-            <a href="http://facebook.com.br">Facebook </a>
-            <a href="http://linkedin.com.br">linkeIn </a>
-            <a href="http://youtube.com.br">Youtube </a>
-        </div>
-    );
-}
-
-function App() {
-    return (
-        <div>
-            <h1>Conheça nossa Equipe:</h1>
-            <Equipe nome="Rafael" cargo="Programador" idade="28" />
-            <Equipe nome="Amanda" cargo="Frond-end" idade="22" />
-            <Equipe nome="Aynara" cargo="Agilista" idade="25" />
-        </div>
-    );
 }
 
 export default App;
