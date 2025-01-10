1aeb5b497ae74c1384f8c0e433cb5c118e98d972
rafael_nunes <rafa.nuns17@gmail.com+>
2025-01-09 22:22:43 -0300

    Trabalhando com listas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
-import Membro from "./components/Membro";
+import Feed from "./components/Feed";
 
 class App extends Component {
-    render(){
-        return(
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+            feed: [
+                { id: 1, username: 'Rafael', likes: 10, comentarios: 2 },
+                { id: 2, username: 'José Campos', likes: 2, comentarios: 0 },
+                { id: 3, username: 'Amanda Cassia', likes: 1200, comentarios: 650 },
+                { id: 4, username: 'Maria Clara', likes: 15000, comentarios: 1800 },
+                { id: 5, username: 'Maria Clara', likes: 0, comentarios: 0 }
+            ]
+
+        };
+    }
+
+    render() {
+        return (
             <div>
-                <Membro nome="Visitante"/>
+                {
+                    this.state.feed.map((item) => {
+                        return (
+                            <Feed id={item.id} username={item.username} likes={item.likes} comentarios={item.comentarios} />
+                        );
+                    })
+                }
             </div>
         );
     }
