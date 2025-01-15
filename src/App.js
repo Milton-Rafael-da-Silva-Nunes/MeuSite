6ef292b4c9470ab539cb5233c60dd3aa8919aafb
rafael_nunes <rafa.nuns17@gmail.com+>
2025-01-14 23:13:59 -0300

    Aprendendo USECALLBACK - USESTATE - USEEFFECT - USEMEMO

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect, useMemo, useCallback } from "react";
 
 function App() {
 
     const [tarefas, setTarefas] = useState([]);
     const [input, setInput] = useState('');
 
+    /*
+    Aqui eu pego as minhas terefas salvas no meu STORAGE
+    pego elas como JSON e converto para lista novamente.
+    */
     useEffect(() => {
         const tarefasStorage = localStorage.getItem('tarefas');
 
@@ -14,17 +18,30 @@ function App() {
 
     }, []);
 
+    /*
+     UseEffect
+     Aqui ele usar o localStorage para criar um mini bd para armazenar minhas tarefas. 
+
+     Ele transforma a LISTA em string usando o JSON.stringfy
+     */
     useEffect(() => {
         localStorage.setItem('tarefas', JSON.stringify(tarefas));
     }, [tarefas]);
 
-    function handleAdd() {
+    /*
+    UseCallback
+    No final que tem o ARRAY eu preciso passar todos os HOOKS que estao sendo usados
+    nessa função (TAREFAS= lista / INOUT= nova tarefa).
+    */
+    const handleAdd = useCallback(() => {
         setTarefas([...tarefas, input]);
         setInput('');
-    }
+    }, [input, tarefas]) ;
 
-    // Use memo executa o LENGTH toda vez que ele for add
-    // Com isso não precisa executar o RETURN toda vez que for add uma tarefa nova.
+    /* 
+    UseMemo executa o LENGTH toda vez que ele for add
+    Com isso não precisa executar o RETURN toda vez que for add uma tarefa nova.
+    */
     const totalTarefas = useMemo(() => tarefas.length, [tarefas]);
 
     return (
